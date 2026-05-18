1cff9dd1b904c11bc612f7201598030532ab619e
sibilr1016 <sibilr1620@gmail.com>
2026-05-18 22:09:25 +0530

    Bug fix

--- frontend/src/layouts/AdminLayout.jsx
+++ frontend/src/layouts/AdminLayout.jsx
@@ -2,6 +2,12 @@ import { useState } from "react";
 import { NavLink, Outlet } from "react-router-dom";
 import { FaArrowRightArrowLeft } from "react-icons/fa6";
 import { GiHamburgerMenu } from "react-icons/gi";
+import {
+  MdOutlineDashboard,
+  MdOutlineProductionQuantityLimits,
+} from "react-icons/md";
+import { LuLogOut, LuUsers } from "react-icons/lu";
+import { BsBagDash } from "react-icons/bs";
 
 function AdminLayout() {
   const [isOpen, setIsOpen] = useState(true); // desktop sidebar
@@ -10,16 +16,17 @@ function AdminLayout() {
   return (
     <div className="flex h-screen bg-gray-50">
       {/* Mobile Overlay */}
-      {isMobileOpen && (
+      {/* {isMobileOpen && (
         <div
           className="fixed inset-0 bg-black/50 z-40 md:hidden"
           onClick={() => setIsMobileOpen(false)}
         />
-      )}
+      )} */}
+      {/* ///////////////////////////////// */}
 
       {/* Sidebar */}
       <aside
-        className={`fixed z-50 top-0 left-0 h-full text-gray-800 shadow-lg
+        className={`fixed bg-green-800 z-50 top-0 transition-all duration-700 left-0 h-full text-gray-800 shadow-lg
         
                 ${isMobileOpen ? "translate-x-0" : "-translate-x-full"}
           md:translate-x-0
@@ -28,18 +35,38 @@ function AdminLayout() {
         
         `}
       >
-        <div className="p-4 font-bold text-lg">Admin</div>
+        <div className="p-4 hidden md:flex font-bold text-lg">Admin</div>
         <nav className="flex flex-col gap-2 p-2">
-          <NavLink to="/dashboard">Dashboard</NavLink>
-          <NavLink to="/pro">Users</NavLink>
-          <NavLink to="/pro">Products</NavLink>
-          <NavLink to="/pro">Logout</NavLink>
+          <NavLink to="/dashboard">
+            <MdOutlineDashboard />
+            <span className={`md:${!isOpen ? "hidden" : "flex"}`}>
+              Dashboard
+            </span>
+          </NavLink>
+          <NavLink to="/pro">
+            <LuUsers />
+            <span className={`md:${!isOpen ? "hidden" : "flex"}`}>Users</span>
+          </NavLink>
+          <NavLink to="/pro">
+            <MdOutlineProductionQuantityLimits />
+            <span className={`md:${!isOpen ? "hidden" : "flex"}`}>
+              Products
+            </span>
+          </NavLink>
+          <NavLink to="/pro">
+            <BsBagDash />
+            <span className={`md:${!isOpen ? "hidden" : "flex"}`}>Orders</span>
+          </NavLink>
+          <NavLink to="/pro">
+            <LuLogOut />
+            <span className={`md:${!isOpen ? "hidden" : "flex"}`}>Logout</span>
+          </NavLink>
         </nav>
       </aside>
 
       {/* Main area */}
       <div
-        className="flex flex-col flex-1"
+        className={`flex flex-col flex-1 ml:${isOpen ? "64" : "16"} transition-all duration-700`}
         style={{ marginLeft: isOpen ? "16rem" : "4rem" }}
       >
         <header>
